a7c59c6932d896e501676b64c3e7dec8d7c497f5
technoknol <technoknol@users.noreply.github.com>
2014-01-24 16:24:49 +0530

    To Disable directory browsing in Wordpress

---
+++ wp-disable-directory-browsing.php
@@ -0,0 +1,14 @@
+<?php
+ 
+ 
+ 
+// To Disable directory browsing in Wordpress 
+// Just one line in your .htaccess file 
+// in your website ROOT 
+ 
+ 
+Options -Indexes
+ 
+ 
+ 
+// ps : Don't remove any other lines. Just add above code at the END
