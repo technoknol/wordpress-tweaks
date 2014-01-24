44b3011fe36b0151e7912248e93cca66c3466520
technoknol <technoknol@users.noreply.github.com>
2014-01-24 19:43:51 +0530

    Create your own shortcode

---
+++ create-own-shortcode.php
@@ -0,0 +1,43 @@
+<?php
+
+/********************************
+
+This code will allow you to create your own shortcode.
+
+
+*************************************/
+
+//  SHORTCODE FOR GOOGLE ADSENSE
+        function google_adsense() {  
+            return '<script type="text/javascript"><!-- 
+            google_ad_client = "<em>your client id</em>"; 
+            google_ad_slot = "<em>your ad slot id</em>"; 
+            google_ad_width = <em>width</em>; 
+            google_ad_height = <em>height</em>; 
+            //--> 
+            </script> 
+            <script type="text/javascript" 
+            src="http://pagead2.googlesyndication.com/pagead/show_ads.js"> 
+            </script> 
+            ';  
+        }  
+          
+        add_shortcode('adsense', 'google_adsense');
+        
+        // USE THIS CODE WHERE YOU WANT TO DISPLAY GOOGLE AD
+        [adsense]
+        
+        
+//  SHORT CODE FOR GIT
+        function show_git($atts) {  
+            extract(shortcode_atts(array('id' => null), $atts));
+          
+          	if( $id != null)   {
+                return '<script src="http://gist.github.com/'.$id.'.js"></script>';
+            }  
+          
+        add_shortcode('gist-git', 'show_git');
+        
+        // USE THIS CODE WHERE YOU WANT TO DISPLAY GIT | Real GIT Example
+        [gist-git id=8559021]
+        
