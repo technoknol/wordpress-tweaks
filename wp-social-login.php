b37accc2e7524f765fb11f34fdfde2e3e8efa066
technoknol <technoknol@users.noreply.github.com>
2014-01-24 18:22:35 +0530

    wordpress login with social sites

---
+++ wp-social-login.php
@@ -0,0 +1,30 @@
+<?php
+
+/*****************************
+
+This is a tweak to customize your wordpress login to allow login with Other Social Sites like Google,Facebook,Twitter,LinkedIn etc...
+
+
+**********************************/
+
+// To enable social logins in your wordpress website 
+// Here some handy plugins available.
+
+    WordPress Social Login      (http://wordpress.org/extend/plugins/wordpress-social-login/)
+    Social Login                (http://wordpress.org/extend/plugins/oa-social-login/)  
+    Social Login for WordPress  (http://wordpress.org/extend/plugins/loginradius-for-wordpress/)
+    Google Apps Login           (http://wordpress.org/plugins/google-apps-login/)
+    Social Login                (http://wordpress.org/plugins/oa-social-login/)
+    Social Connect              (http://wordpress.org/plugins/social-connect/)
+    
+
+//  Some More     
+    
+    Simple Facebook Connect
+    Simple Twitter Connect
+    Simple Google Connect
+    WP Google Plus Connect
+
+
+
+Source : http://wp.smashingmagazine.com/2012/05/29/login-wordpress-using-social-network/
